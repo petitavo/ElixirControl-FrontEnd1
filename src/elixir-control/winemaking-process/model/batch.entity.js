2948b09beb5853c6b1602cc57a0ecc1ea5e571bd
Janover Saldaña <u20201b510@upc.edu.pe>
2024-11-19 03:25:45 -0500

    feat(winemaking-process): Information on the entities that correspond to the winemaking process has been updated.

--- src/elixir-control/winemaking-process/model/batch.entity.js
+++ src/elixir-control/winemaking-process/model/batch.entity.js
@@ -1,17 +1,17 @@
 
 export class Batch {
 
-    constructor({ id='', grape_variety='', harvest_date='',
-                    grape_quantity='', vineyard_origin='', current_status='',
-                    process_start_date='', final_volume=''
-    }) {
+
+    constructor({id, profileId, vineyardCode, grapeVariety, harvestDate, grapeQuantity, vineyardOrigin, processStartDate}) {
         this.id = id;
-        this.grape_variety = grape_variety;
-        this.harvest_date = harvest_date;
-        this.grape_quantity = grape_quantity;
-        this.vineyard_origin = vineyard_origin;
-        this.current_status = current_status;
-        this.process_start_date = process_start_date;
-        this.final_volume = final_volume;
+        this.profileId = profileId;
+        this.vineyardCode = vineyardCode;
+        this.grapeVariety = grapeVariety;
+        this.harvestDate = harvestDate;
+        this.grapeQuantity = grapeQuantity;
+        this.vineyardOrigin = vineyardOrigin;
+        this.processStartDate = processStartDate;
     }
+
+    //ELIMINAR VOLUMEN FINAL DEL PROYECTO Y DE LA ENTIDAD BATCH
 }
--- src/elixir-control/winemaking-process/model/pressing.entity.js
+++ src/elixir-control/winemaking-process/model/pressing.entity.js
@@ -1,13 +1,21 @@
 
 export class Pressing {
-    constructor({id='', batch_id='', pressing_date='', must_volume='',
-                    press_type='', applied_pressure=''
-    }) {
+
+    //"id": 0,
+    //"batchId": 0,
+    //"pressingDate": "2024-11-19T08:23:01.051Z",
+    //"mustVolume": 0,
+    //"pressType": "string",
+    //"appliedPressure": 0
+
+    constructor({ id='', batch_id='', pressing_date='', must_volume='', press_type='', applied_pressure='' }) {
+
         this.id = id;
         this.batch_id = batch_id;
         this.pressing_date = pressing_date;
         this.must_volume = must_volume;
         this.press_type = press_type;
         this.applied_pressure = applied_pressure;
+
     }
 }
